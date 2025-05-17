3d4c795a97b971c5b118ed6dd3610a7cde781e7a
Romi Pereira <romipermont@gmail.com>
2025-05-18 11:20:08 +1200

    log img added
    Logo image for 1 news added and working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
     return (
       <header className="site-header">
         <div className="left-icons">☰</div>
-        <img src="/logo.svg" alt="1news logo" className="logo" />
+        <img src="/logo.svg" alt="1News logo" className="logo" />
         <div className="right-icons">
           <input type="text" placeholder="Search..." />
           <span>🔍</span>
