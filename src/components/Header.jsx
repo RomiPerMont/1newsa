d4f4288868b246e3cd6486e74b06cea4b2a6c807
Romi Pereira <romipermont@gmail.com>
2025-05-18 17:18:36 +1200

    Added weather components
    Added weather component but with manual changes. Still needs to be worked for a live weather updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,25 @@
-function Header() {
-    return (
-      <header className="site-header">
-        <div className="left-icons">☰</div>
-        <img src="/logo.svg" alt="1News logo" className="logo" />
+import Weather from './Weather';
 
-        <div className="right-icons">
-          <input type="text" placeholder="Search..." />
-          <span>🔍</span>
-          <span>🤍</span>
-          <span>👤</span>
-        </div>
-      </header>
-    );
-  }
-  
-  export default Header;
-  
+function Header() {
+  return (
+    <header className="site-header">
+      <div className="left-block">
+        <Weather /> 
+        <div className="left-icons">☰</div>
+      </div>
+
+      <div className="center-block">
+        <img src="/logo.svg" alt="1News logo" className="logo" />
+      </div>
+
+      <div className="right-icons">
+        <input type="text" placeholder="Search..." />
+        <span>🔍</span>
+        <span>🤍</span>
+        <span>👤</span>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Weather.jsx
@@ -0,0 +1,18 @@
+function Weather() {
+    const forecast = {
+      city: "Auckland",
+      condition: "Mostly sunny",
+      temp: "  18°C"
+    };
+  
+    return (
+      <div className="weather">
+        <p>📍 {forecast.city},
+        🌤️ {forecast.condition},   
+          {forecast.temp}</p>
+      </div>
+    );
+  }
+  
+  export default Weather;
+  
