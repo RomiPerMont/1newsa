f08f623dcc6493b7cc9596dc7ab1445418b21832
Romi Pereira <romipermont@gmail.com>
2025-05-21 23:16:38 +1200

    Likes section clickable
    Likes session is now clickable and moves you to the next page. Some problems with the images disappearing were solved

---
+++ src/components/LikesPage.jsx
@@ -0,0 +1,10 @@
+function LikesPage() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h2>❤️ Likes</h2>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
+
+export default LikesPage;
